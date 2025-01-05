1c7782b3fc453e4ef295feb74903538cfdb02083
Nic Delorme <nic.delorme@nrdtech.io>
2025-01-05 12:11:52 -0600

    improvements

--- terraform/main/lambda.tf
+++ terraform/main/lambda.tf
@@ -12,11 +12,11 @@
 # }
 
 resource "aws_lambda_function" "lambda_function" {
-  depends_on = [docker_registry_image.hash_image]
+  depends_on = [null_resource.push_image]
   function_name = var.app_ident
   role          = aws_iam_role.lambda_exec.arn
   package_type  = "Image"
-  image_uri = "${aws_ecr_repository.ecr_repository.repository_url}:${docker_image.terraform_function_image.triggers.code_hash}"
+  image_uri = "${aws_ecr_repository.ecr_repository.repository_url}:${null_resource.push_image.triggers.code_hash}"
   timeout = var.app_timeout
   memory_size = var.app_memory
 
--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.81.0"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
   }
 }
 
