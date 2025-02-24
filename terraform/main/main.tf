388971263cfebac85c7bdc7813d8085bca10495f
Nic Delorme <nic.delorme@nrdtech.io>
2025-02-24 06:46:45 -0600

    improve custom vpc config system

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -27,11 +27,24 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 #############################
-# Default VPC
+# VPC
 #############################
+# Custom VPC
+# locals {
+#   vpc_name = "my-standard-vpc"
+# }
+# data "aws_vpc" "selected" {
+#   filter {
+#     name   = "tag:Name"
+#     values = [local.vpc_name]
+#   }
+# }
+
+# Default VPC
 data "aws_vpc" "selected" {
   default = true
 }
+
 data "aws_subnets" "subnets" {
   filter {
     name   = "vpc-id"
@@ -58,35 +71,14 @@ data "aws_subnets" "private" {
     values = ["*private*"]
   }
 }
+data "aws_route_tables" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
 
-#############################
-# Custom VPC
-#############################
-# VPC
-# variable "vpc_name" {
-#   type = string
-# }
-# data "aws_vpc" "selected" {
-#   filter {
-#     name   = "tag:Name"
-#     values = [var.vpc_name]
-#   }
-# }
-# 
-# # Subnets
-# data "aws_subnet_ids" "all" {
-#   vpc_id = data.aws_vpc.selected.id
-# }
-# data "aws_subnet_ids" "private" {
-#   vpc_id = data.aws_vpc.selected.id
-#   tags = {
-#     Name = "*private*"
-#   }
-# }
-# data "aws_subnet_ids" "public" {
-#   vpc_id = data.aws_vpc.selected.id
-#   tags = {
-#     Name = "*public*"
-#   }
-# }
-#######################################
+  filter {
+    name   = "association.subnet-id"
+    values = data.aws_subnets.private.ids
+  }
+}
